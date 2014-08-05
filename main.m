01bdc9a00d16692cc797e030e0a73828ac6306b2
Emmanuel Ngenoh <ekngen11@gmail.com>
2014-08-05 10:16:24 -0400

    main.m

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PdfViewer
+//
+//  Created by Emmanuel Ngeno on 9/12/13.
+//  Copyright (c) 2013 Emmanuel Ngeno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
